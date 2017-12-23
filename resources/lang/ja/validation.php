5bc645720a3b3fc3868e6d39e587121dc7ae7bee
上田雄貴 <yuuki9552@gmail.com>
2017-12-24 02:41:33 +0900

    バリデーションメッセージの変更

--- resources/lang/ja/validation.php
+++ resources/lang/ja/validation.php
@@ -65,7 +65,7 @@ return [
         'array'   => 'The :attribute must have at least :min items.',
     ],
     'not_in'               => 'The selected :attribute is invalid.',
-    'numeric'              => 'The :attribute must be a number.',
+    'numeric'              => '数値以外を入力しないでください',
     'present'              => 'The :attribute field must be present.',
     'regex'                => 'The :attribute format is invalid.',
     'required'             => ':attributeを入力してください。',
@@ -87,6 +87,7 @@ return [
     'unique'               => 'The :attribute has already been taken.',
     'uploaded'             => 'The :attribute failed to upload.',
     'url'                  => 'The :attribute format is invalid.',
+    'request_exist'         => ':attributeは重複登録できません',
 
     /*
     |--------------------------------------------------------------------------
