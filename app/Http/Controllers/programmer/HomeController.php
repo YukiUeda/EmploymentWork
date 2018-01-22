dc6dd9ff28f575727a5a4b05fb101be5d2410b4a
上田雄貴 <yuuki9552@gmail.com>
2018-01-23 06:16:18 +0900

    プログラマーのTOPに報酬チャートを追加

--- app/Http/Controllers/programmer/HomeController.php
+++ app/Http/Controllers/programmer/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\programmer;
 
+use App\ProductClick;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -19,6 +20,7 @@ class HomeController extends Controller
     }
 
     /**
+     * TOP画面の表示
      * Show the application dashboard.
      *
      * @return \Illuminate\Http\Response
@@ -26,7 +28,92 @@ class HomeController extends Controller
     public function index()
     {
         $programmer = \Auth::user();
-        $name = $programmer->name;
-        return view('programmer.top',compact("name"));
+        //日別のクリック報酬の取得
+        $productClicks= ProductClick::query()
+            ->select(\DB::raw('sum(click_price) as price, DATE_FORMAT(product_clicks.created_at, \'%Y%-%m%-%d\') as date'))
+            ->join('curriculums','curriculum_id','=','curriculums.id')
+            ->join('products','products.id','=','curriculums.product_id')
+            ->groupBy(\DB::raw('DATE_FORMAT(product_clicks.created_at, \'%Y%-%m%-%d\')'))
+            ->where('curriculums.programmer_id','=',$programmer->id)
+            ->get();
+
+        $data = array();
+        $labels = array();
+
+        //チャートにセットするために整形
+        foreach ($productClicks as $productClick){
+            $labels[] = $productClick->date;
+            $data[]   = ( int )$productClick->price;
+        }
+
+        //jsで使うためにjson型にエンコード
+        $labels = json_encode($labels);
+        $data = json_encode($data);
+
+        return view('programmer.top',compact('data','labels'));
+    }
+
+    /**
+     * ajaxで報酬のチャートデータ取得
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function ajax(Request $request){
+        $programmer = \Auth::user();
+        //dateのタイプ取得
+        $date = $request->date;
+        //タイプ毎に取得するデータの分岐
+        switch ($date){
+            case 'year':
+                //1年単位のデータの取得
+                $type = '年';
+                $productClicks= ProductClick::query()
+                    ->select(\DB::raw('sum(click_price) as price, DATE_FORMAT(product_clicks.created_at, \'%Y\') as date'))
+                    ->join('curriculums','curriculum_id','=','curriculums.id')
+                    ->join('products','products.id','=','curriculums.product_id')
+                    ->groupBy(\DB::raw('DATE_FORMAT(product_clicks.created_at, \'%Y\')'))
+                    ->where('curriculums.programmer_id','=',$programmer->id)
+                    ->get();
+                break;
+            case 'month':
+                //１月単位のデータの取得
+                $type = '月';
+                $productClicks= ProductClick::query()
+                    ->select(\DB::raw('sum(click_price) as price, DATE_FORMAT(product_clicks.created_at, \'%Y%-%m\') as date'))
+                    ->join('curriculums','curriculum_id','=','curriculums.id')
+                    ->join('products','products.id','=','curriculums.product_id')
+                    ->groupBy(\DB::raw('DATE_FORMAT(product_clicks.created_at, \'%Y%-%m\')'))
+                    ->where('curriculums.programmer_id','=',$programmer->id)
+                    ->get();
+                break;
+            case 'day':
+                //１日単位の日にちの取得
+                $productClicks= ProductClick::query()
+                    ->select(\DB::raw('sum(click_price) as price, DATE_FORMAT(product_clicks.created_at, \'%Y%-%m%-%d\') as date'))
+                    ->join('curriculums','curriculum_id','=','curriculums.id')
+                    ->join('products','products.id','=','curriculums.product_id')
+                    ->groupBy(\DB::raw('DATE_FORMAT(product_clicks.created_at, \'%Y%-%m%-%d\')'))
+                    ->where('curriculums.programmer_id','=',$programmer->id)
+                    ->get();
+                break;
+        }
+
+        $data = array();
+        $labels = array();
+
+        foreach ($productClicks as $productClick){
+            $labels[] = $productClick->date;
+            $data[]   = ( int )$productClick->price;
+        }
+        //チャートにセットするために整形
+        $date = array(
+            'labels' => $labels,
+            'datasets'=>array(
+                'label'=>'１'.$type.'毎の報酬集計(円)',
+                'data'=>$data
+            )
+        );
+        //Json型にエンコードしてリターン
+        return \Response::json($date);
     }
 }
---
+++ resources/views/programmer/top.blade.php
@@ -0,0 +1,98 @@
+@extends('programmer.layout')
+
+@section('layout_js')
+    <script type="text/javascript" src="{{{'/js/chart/Chart.min.js'}}}"></script>
+    <script type="text/javascript" src="{{{'/js/programmer/chart.js'}}}"></script>
+
+@endsection
+@section('content')
+    <div class="main">
+        <h2>１日毎の報酬集計(円)</h2>
+        <div class="button_group">
+            <button data-date="year" class="left-button">年</button>
+            <button data-date="month">月</button>
+            <button data-date="day" id="active_button" class="right-button">日</button>
+        </div>
+        <canvas id="myChart"></canvas>
+        <script>
+            $(function() {
+                var data = {!! $data !!};
+                var labels = {!! $labels !!};
+                var ctx = document.getElementById('myChart').getContext('2d');
+                var myChart = new Chart(ctx, {
+                    type : 'line',
+                    data: {
+                        labels: labels,
+                        datasets:[{
+                            label:"１日毎の報酬集計(円)",
+                            fill :false,
+                            pointBackgroundColor:'rgb(238, 110, 115)',
+                            pointRadius:5,
+                            borderColor: "rgb(238, 110, 115)",//線の色
+                            data : data ,
+                        }]
+                    },
+                    options:{
+                        scales:{
+                            yAxes:[{
+                                ticks: {
+                                    beginAtZero: true,
+                                    min:0,
+                                }
+                            }]
+                        }
+                    }
+                });
+
+                $('.button_group button').on('click',function() {
+                    var date = $(this);
+                    $.ajax({
+                        headers: {
+                            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                        },
+                        url: '/programmer/chart/ajax',
+                        type: 'POST',
+                        data:{
+                            'date':$(this).data('date')
+                        }
+                    })
+                        .done(function(data) {
+                            labels = data['labels'];
+                            var ctx = document.getElementById('myChart').getContext('2d');
+                            myChart.destroy();
+                            myChart = new Chart(ctx, {
+                                type : 'line',
+                                data: {
+                                    labels:data['labels'],
+                                    datasets:[{
+                                        pointRadius:5,
+                                        data :data['datasets']['data'],
+                                        label:data['datasets']['label'],
+                                        fill :false,
+                                        borderColor:'rgb(238, 110, 115)',
+                                        pointBackgroundColor:'rgb(238, 110, 115)'
+                                    }]
+                                },
+                                options:{
+                                    scales:{
+                                        yAxes:[{
+                                            ticks: {
+                                                beginAtZero: true,
+                                                min:0,
+                                            }
+                                        }]
+                                    }
+                                }
+                            });
+                            $('.button_group button').removeAttr('id');
+                            $(date).attr('id','active_button');
+                            $('h2').text(data['datasets']['label']);
+                        })
+                        .fail(function(data) {
+                            $('.result').html(data);
+                        });
+                })
+            });
+        </script>
+    </div>
+@endsection
