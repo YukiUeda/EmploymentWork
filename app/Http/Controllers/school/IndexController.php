ffb873a16d1bb1fada640094efe890ab78846c73
上田雄貴 <yuuki9552@gmail.com>
2018-01-23 11:24:07 +0900

    TOPに新着ニュース追加

--- app/Http/Controllers/school/IndexController.php
+++ app/Http/Controllers/school/IndexController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers\school;
 
 use App\Http\Controllers\Controller;
+use App\School;
+use App\SchoolCurriculum;
+use App\TeacherAccount;
+use DebugBar\DebugBar;
 use Illuminate\Http\Request;
 use App\SchoolTimeTable;
 
@@ -11,7 +15,32 @@ class IndexController extends Controller
     public function index()
     {
         $school = \Auth::user();
-        $name = $school->name;
-        return view('school.top',compact('name'));
+        $sId     = $school->id;
+
+        //教師が登録したカリキュラムの新着情報
+        $newses = School::query()
+            ->select('teacher_accounts.name','title','date','time_table_id','school_curriculums.updated_at')
+            ->join('teacher_accounts','teacher_accounts.school_id','=','schools.id')
+            ->join('school_curriculums','school_curriculums.teacher_id','=','teacher_accounts.id')
+            ->join('curriculums','curriculums.id','=','school_curriculums.curriculum_id')
+            ->where('schools.id','=',$sId)
+            ->orderBy('school_curriculums.updated_at')->get();
+
+        //学校の先生情報取得
+        $teachers = TeacherAccount::query()->where('teacher_accounts.school_id','=',$sId)
+            ->select(\DB::raw('count(school_curriculums.id) as num,teacher_accounts.*,school_grade,class_name'))
+            ->leftJoin('school_curriculums',function ($join) {
+                $join->on('school_curriculums.teacher_id','=','teacher_accounts.id')
+                    ->where('school_curriculums.year','=',date('Y',strtotime('-3 month',time())));
+            })
+            ->leftJoin('teacher_classes',function ($join){
+                $join->on('teacher_classes.teacher_id','=','teacher_accounts.id')
+                    ->where('teacher_classes.year','=',date('Y',strtotime('-3 month',time())));
+            })
+            ->whereNull('deleted_at')
+            ->groupBy('teacher_accounts.id','teacher_classes.id')
+            ->get();
+
+        return view('school.top',compact('teachers','newses'));
     }
 }
--- resources/views/school/top.blade.php
+++ resources/views/school/top.blade.php
@@ -1,5 +1,42 @@
 @extends('school.layout')
 
 @section('content')
+    <div  class="row">
+        <div class="col s12">
+            <h2>新着ニュース</h2>
+            <div class="card-panel">
+                <div class="row">
+                    @foreach($newses as $news)
+                        <div class="col s12">
+                            <p class="col s2">{{$news->updated_at}}</p>
+                            <p class="col s10">{{$news->name}}先生が{{$news->title}}を{{$news->date}}の{{$news->time_table_id}}時限目に登録しました。</p>
+                        </div>
+                        @if(3 < $loop->count)
+                            @break
+                        @endif
+                    @endforeach
+                </div>
+            </div>
 
+            <h2>所属教師</h2>
+            <table class="highlight">
+                <thead>
+                <tr>
+                    <th>名前</th>
+                    <th>担当クラス</th>
+                    <th>カリキュラム登録数</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($teachers as $teacher)
+                    <tr>
+                        <td>{{$teacher->name}}</td>
+                        <td>{{isset($teacher->class_name) ? $teacher->school_grade.'年'.$teacher->class_name.'組' : '担当クラスなし'}}</td>
+                        <td>{{$teacher->num}}時間</td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
 @endsection
