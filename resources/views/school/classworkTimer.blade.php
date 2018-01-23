c470c5df70e9e642cb4b3c3a12cd4005875148f1
上田雄貴 <yuuki9552@gmail.com>
2018-01-23 11:29:51 +0900

    classの変更

--- resources/views/school/classworkTimer.blade.php
+++ resources/views/school/classworkTimer.blade.php
@@ -6,22 +6,22 @@
         @foreach($table as $lesson)
             <div class="col s6">
                 {!! Form::label('lesson'.$i.'_str', $i.'時間目開始') !!}
-                {!! Form::text('lesson'.$i.'_str', $lesson['start'],['class'=>'textpicker']) !!}
+                {!! Form::text('lesson'.$i.'_str', $lesson['start'],['class'=>'timepicker']) !!}
             </div>
             <div class=" col s6">
                 {!! Form::label('lesson'.$i.'_end', $i.'時間目終了') !!}
-                {!! Form::text('lesson'.$i.'_end', $lesson['end'],['class'=>'textpicker']) !!}
+                {!! Form::text('lesson'.$i.'_end', $lesson['end'],['class'=>'timepicker']) !!}
             </div>
             <?php $i++ ?>
         @endforeach
         @for(;$i<7;$i++)
             <div class="col s6">
                 {!! Form::label('lesson'.$i.'_str', $i.'時間目開始') !!}
-                {!! Form::text('lesson'.$i.'_str', null,['class'=>'textpicker']) !!}
+                {!! Form::text('lesson'.$i.'_str', null,['class'=>'timepicker']) !!}
             </div>
             <div class=" col s6">
                 {!! Form::label('lesson'.$i.'_end', $i.'時間目終了') !!}
-                {!! Form::text('lesson'.$i.'_end', null,['class'=>'textpicker']) !!}
+                {!! Form::text('lesson'.$i.'_end', null,['class'=>'timepicker']) !!}
             </div>
         @endfor
     </div>
